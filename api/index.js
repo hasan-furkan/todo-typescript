98ac33f0db7da12c3262c5689ca914a38a402229
hasan-furkan <hsnfrkn32@gmail.com>
2023-08-07 09:33:59 +0300

    todo initial commit

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+const todoRouter = require('./routes/todo');
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+app.use('/api/v1.0/todos', todoRouter);
 
-app.listen(4000, () => {
-    console.log('Example app listening on port 3000!')
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
