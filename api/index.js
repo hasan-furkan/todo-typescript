f3ebf70b560d5c48427f849fc5bd30cfda034456
hasan-furkan <hsnfrkn32@gmail.com>
2023-08-09 02:31:03 +0300

    server.js duzenlendi

--- api/index.js
+++ api/index.js
@@ -1,54 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+const app = require("./server");
 const config = require("./config");
-const { Sequelize } = require("sequelize");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const port = config.port;
 
-const { sequelize } = require("./models");
-const { startRedis } = require("./redisClient");
-const todoRouter = require("./routes/todo");
-const authRouter = require("./routes/auth");
-
-app.use(express.json());
-app.use(cors());
-
-app.use("/api/v1.0/todos", todoRouter);
-app.use("/api/v1.0/auth", authRouter);
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-const connectDb = new Sequelize(
-  "postgres",
-  config.db.user,
-  config.db.password,
-  {
-    host: config.db.host,
-    dialect: "postgres",
-    port: config.db.port,
-    username: config.db.user,
-    password: config.db.password,
-    database: config.db.database,
-  }
-);
-
-try {
-  if (connectDb) {
-    sequelize
-      .sync()
-      .then(() => {
-        console.log("Database synced");
-        return connectDb.authenticate();
-      })
-      .then(() => {
-        console.log(`Connected to ${config.db.database} database`);
-        startRedis();
-      });
-  } else {
-    console.log(`Failed to connect to ${config.db.database} database`);
-  }
-} catch (err) {
-  console.log("Error: ", err);
-}
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- api/tests/auth.routes.test.js
+++ api/tests/auth.routes.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../index");
+const app = require("../server");
 const { User } = require("../models");
 const redisClient = require("../redisClient");
 const {
