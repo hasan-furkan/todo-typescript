2bfd32c968bde09a8f58bf6f5e2067e05314766a
hasan-furkan <hsnfrkn32@gmail.com>
2023-08-09 02:20:18 +0300

    todos test tamamlandi

--- api/tests/todo.routes.test.js
+++ api/tests/todo.routes.test.js
@@ -45,3 +45,130 @@ describe("GET /todos", () => {
     });
   }, 10000);
 });
+
+describe("POST /todos", () => {
+  it("should handle create todo", async () => {
+    const mockTodo = {
+      id: 1,
+      title: "test",
+      completed: false,
+      userId: 1,
+    };
+    Todo.create.mockResolvedValue(mockTodo);
+    tokenValidatorMiddleware.mockImplementation((req, res, next) => {
+      req.userObj = { id: 1 }; // Bu id'yi mockTodo'dan alabilirsiniz
+      next();
+    });
+
+    const res = await request(app).post("/api/v1.0/todos").send({
+      title: "test",
+      completed: false,
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({
+      status: true,
+      message: "todo added",
+      data: {
+        id: mockTodo.id,
+        title: mockTodo.title,
+        completed: mockTodo.completed,
+        userId: mockTodo.userId,
+      },
+    });
+
+    expect(Todo.create).toHaveBeenCalledWith({
+      title: "test",
+      completed: false,
+      UserId: mockTodo.userId,
+    });
+  }, 10000);
+});
+
+describe("PUT /todos/:id", () => {
+  it("should handle update todo", async () => {
+    const mockTodo = {
+      id: 1,
+      title: "test",
+      completed: false,
+      userId: 1,
+    };
+    Todo.update.mockResolvedValue(mockTodo);
+    tokenValidatorMiddleware.mockImplementation((req, res, next) => {
+      req.userObj = { id: 1 }; // Bu id'yi mockTodo'dan alabilirsiniz
+      next();
+    });
+
+    const res = await request(app).put("/api/v1.0/todos/1").send({
+      title: "test",
+      completed: false,
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({
+      status: true,
+      message: "todo updated",
+      data: {
+        id: mockTodo.id,
+        title: mockTodo.title,
+        completed: mockTodo.completed,
+        userId: mockTodo.userId,
+      },
+    });
+
+    expect(Todo.update).toHaveBeenCalledWith(
+      {
+        title: "test",
+        completed: false,
+      },
+      {
+        where: {
+          id: "1",
+          UserId: mockTodo.userId,
+        },
+      }
+    );
+  }, 10000);
+});
+
+describe("DELETE /todos/:id", () => {
+  it("should handle delete todo", async () => {
+    const mockTodo = {
+      id: 1,
+      title: "test",
+      completed: false,
+      userId: 1,
+    };
+    Todo.update.mockResolvedValue(mockTodo);
+    tokenValidatorMiddleware.mockImplementation((req, res, next) => {
+      req.userObj = { id: 1 }; // Bu id'yi mockTodo'dan alabilirsiniz
+      next();
+    });
+
+    const res = await request(app).delete("/api/v1.0/todos/1");
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({
+      status: true,
+      message: "todo deleted",
+      data: {
+        id: mockTodo.id,
+        title: mockTodo.title,
+        completed: mockTodo.completed,
+        userId: mockTodo.userId,
+      },
+    });
+
+    expect(Todo.update).toHaveBeenCalledWith(
+      {
+        isDeleted: true,
+      },
+      {
+        where: {
+          id: "1",
+          UserId: mockTodo.userId,
+        },
+      }
+    );
+  }, 10000);
+});
